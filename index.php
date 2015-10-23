159119e31027aafec73a0ef7c24a206bdef76b26
RryLee <qq215672398@126.com>
2015-10-23 10:42:43 +0800

    project init

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>message</title>
+    <link rel="stylesheet" type="text/css" href="http://cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <style>
+        body {
+            margin-top: 2em;
+        }
+    </style>
+</head>
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <p class="text-muted">The recent message.</p>
+                <div class="messages">
+                    <div class="message">
+                        <p><b>RryLee: </b>hi!</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-6">
+                <form action="api/post.php">
+                    <!-- Name field -->
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input class="form-control" name="name" id="name" value="">
+                    </div>
+
+                    <!-- Message field -->
+                    <div class="form-group">
+                        <label for="message">Message</label>
+                        <textarea name="message" id="message" class="form-control"></textarea>
+                    </div>
+
+                    <input type="submit" class="btn btn-default" value="Send">
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <script src="http://cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
+    <script src="js/app.js"></script>
+</body>
+</html>
